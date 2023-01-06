d38bfa8c73b4ff31ec6f1454f9101167ba4f6909
AndreRRodrigues <83727384+Rayjir@users.noreply.github.com>
2023-01-06 14:19:27 -0300

    06/01

--- lib/Telas/CadastroUsuarios.dart
+++ lib/Telas/CadastroUsuarios.dart
@@ -9,13 +9,60 @@ class CadastroUsuario extends StatefulWidget {
   State<CadastroUsuario> createState() => _CadastroUsuarioState();
 }
 
+final FormKey = GlobalKey<FormState>();
+
+TextEditingController NomeController = TextEditingController();
+TextEditingController EmailController = TextEditingController();
+TextEditingController SenhaController = TextEditingController();
+
 class _CadastroUsuarioState extends State<CadastroUsuario> {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        
+        home: Scaffold(
+      body: Form(
+        key: FormKey,
+        child: Container(
+          alignment: Alignment.center,
+            color: Colors.amber,
+            child: Container(
+              alignment: Alignment.center,
+              height: MediaQuery.of(context).size.height * 0.8 ,
+              width: MediaQuery.of(context).size.width * 0.3,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                    child: TextFormField(
+                      controller: NomeController,
+                      decoration: const InputDecoration(
+                        hintText: "Digite o nome do usuario",
+                        labelText:"Nome do funcionario",
+                        prefixIcon: Icon(Icons.person)
+                      ),
+                    )),
+                    Padding(padding:EdgeInsets.fromLTRB(10, 15, 10, 15),
+                    child: TextFormField(
+                      controller: EmailController,
+                      decoration: const InputDecoration(
+                        hintText: "Dinite o email de acesso",
+                        labelText:"Email",
+                        prefixIcon: Icon(Icons.email)
+                      )
+                    )),
+                    Padding(padding:EdgeInsets.fromLTRB(10, 15, 10, 15),
+                    child:TextFormField(controller: SenhaController,
+                    decoration: const InputDecoration(
+                      hintText:"Digite a senha do usuario",
+                      labelText: "Senha",
+                      prefixIcon: Icon(Icons.lock)
+                    )))
+              ]),
+            )),
       ),
-    );
+    ));
   }
 }
